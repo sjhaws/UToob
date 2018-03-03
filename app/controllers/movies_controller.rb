428367f0a8be19796cab44ec7e0ce6362d4793b3
juanaguiniga <juanaguiniga@ymail.com>
2018-03-03 10:22:32 -0700

    movie controller update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,19 @@
 class MoviesController < ApplicationController
+  before_action :movie_routes, only: [:show, :update, :edit, :destroy]
+  
   def index
+    @movies = Movie.all
   end
 
   def show
-  end
+    end
 
   def new
+    @movie = Movie.new
   end
 
   def edit
   end
+
+
 end
