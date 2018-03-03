854373efe21ae9cd0ab4593efb0983eef623b5ce
Steven Haws <stevenjhaws@gmail.com>
2018-03-03 10:18:54 -0700

    added routes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,6 @@
 class MoviesController < ApplicationController
+  before_action :movie_routes, only: [:show, :update, :edit, :destroy]
+  
   def index
   end
 
@@ -10,4 +12,16 @@ class MoviesController < ApplicationController
 
   def edit
   end
+
+  def update
+  
+  end
+
+  def destroy
+  end
+
+  private
+    def movie_routes
+      @movie = Movie.find(params[:id])
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20180303164828) do
     t.string "trailer"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.datetime "created_at", null: false
