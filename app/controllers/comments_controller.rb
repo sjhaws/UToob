1cc07cf2de878346406e9e1073c3d8396fd80885
rosinrae <rosalynrdecker@gmail.com>
2018-03-03 14:26:22 -0700

    Fleshed out model integration

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,15 @@
 class CommentsController < ApplicationController
 
   before_action :set_movie
+  before_action :set_comment, only: [:edit, :update, :destroy]
 
   def new
     @comment = Comment.new(user_id: current_user.id)
+    render partial: "form"
+  end
+
+  def edit
+    render partial: "form"
   end
 
   def show
@@ -19,7 +25,29 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    render partial: "form"
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to @movie
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to @movie
+  end
+
   private
+
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
     def set_movie
       @movie = Movie.find(params[:movie_id])
     end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   }
 
   scope 'movies/:movie_id', as: 'movie' do
-    resources :comments, only: [:new, :create, :delete]
+    resources :comments, only: [:new, :create, :edit, :update, :destroy]
   end
 
 end
